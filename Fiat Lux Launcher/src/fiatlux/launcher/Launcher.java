55cb0c0500a35f17e1df4838b7d823bdcde7c0fd
orehmane <orehmane@gmail.com>
2011-12-14 00:40:37 +0000

    - Removed the JAR files. - Launcher now prints some debug information to the terminal.

--- Fiat Lux Launcher/src/fiatlux/launcher/Launcher.java
+++ Fiat Lux Launcher/src/fiatlux/launcher/Launcher.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 public class Launcher {
 	public static void main(String[] args) {
 		try {
+			System.out.println(System.getProperty("java.version"));
 			int version = System.getProperty("java.version").charAt(2);
+			System.out.println(version);
 
 			// if java is version 5 or higher, run standard version
 			if (version > 5) {
